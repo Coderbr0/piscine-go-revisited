add57a1fd3a1ae4b7c341a930e88e6f92e189532
Coderbro <mo.abbaszadi@gmail.com>
2022-05-05 18:04:11 +0100

    printalphabet

--- Quest2/printalphabet/main.go
+++ Quest2/printalphabet/main.go
@@ -6,9 +6,29 @@ func main() {
 	alphabet := "abcdefghijklmnopqrstuvwxyz"
 
 	for i := 0; i <= 25; i++ {
+		z01.PrintRune(rune(alphabet[i])) // alphabet[i] converts the string to bytes so we cast it to runes for PrintRune function of z01 package to work
+	}
+	z01.PrintRune('\n')
+}
+
+/* Alternatives:
+
+func main() {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+
+	for i := range alphabet {
 		z01.PrintRune(rune(alphabet[i]))
 	}
 	z01.PrintRune('\n')
 }
 
-// alphabet[i] converts the string to bytes so we cast it to runes for PrintRune function of z01 package to work
+func main() {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+
+	for _, v := range alphabet {
+		z01.PrintRune(v)
+	}
+	z01.PrintRune('\n')
+}
+
+*/
