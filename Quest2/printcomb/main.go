a40bf905b0ece5d6c67aa5d391242486f9ea778a
Coderbro <mo.abbaszadi@gmail.com>
2022-05-10 21:12:18 +0100

    printcomb

--- Quest2/printcomb/main.go
+++ Quest2/printcomb/main.go
@@ -1,14 +1,24 @@
-package main
+package main // package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
-	for a := '7'; b := '8'; c := '9' {
-		if a < b && b < c {
-			z01.PrintRune(rune(a))
-			z01.PrintRune(rune(b))
-			z01.PrintRune(rune(c))
-			z01.PrintRune('\n')
+func main() { // func PrintComb() {}
+	for a := 48; a < 56; a++ {			// a starts at 48 as rune character value for this integer is 0; refer to ascii table (man ascii in terminalc)
+		for b := a + 1; b < 57; b++ {
+			for c := b + 1; c < 58; c++ {
+				if a == 55 && b == 56 && c == 57 {
+					z01.PrintRune(rune(a))
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(c))
+					z01.PrintRune('\n')
+				} else {
+					z01.PrintRune(rune(a))
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(c))
+					z01.PrintRune(44) // Comma in ascii table
+					z01.PrintRune(32) // Space in ascii table
+				}
+			}
 		}
 	}
 }
