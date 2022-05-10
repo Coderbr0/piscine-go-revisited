e6d58838b1706cfd0cb6485747522d6f88a167b1
Coderbro <mo.abbaszadi@gmail.com>
2022-05-10 21:14:23 +0100

    printcomb

--- Quest2/printcomb/main.go
+++ Quest2/printcomb/main.go
@@ -6,7 +6,7 @@ func main() { // func PrintComb() {}
 	for a := 48; a < 56; a++ {			// a starts at 48 as rune character value for this integer is 0; refer to ascii table (man ascii in terminal)
 		for b := a + 1; b < 57; b++ {
 			for c := b + 1; c < 58; c++ {
-				if a == 55 && b == 56 && c == 57 {
+				if a == 55 && b == 56 && c == 57 {	// 789
 					z01.PrintRune(rune(a))
 					z01.PrintRune(rune(b))
 					z01.PrintRune(rune(c))
