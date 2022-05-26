5f44793377ff8ac2454e14c63688392d0de12b02
Coderbro <mo.abbaszadi@gmail.com>
2022-05-26 20:10:55 +0100

    atoi.go

---
+++ Quest3/atoi.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Atoi(s string) int {
+	y, _ := strconv.Atoi(s)
+	return y
+}
+
+func main() {
+	fmt.Println(Atoi("12345"))
+	fmt.Println(Atoi("0000000012345"))
+	fmt.Println(Atoi("012 345"))
+	fmt.Println(Atoi("Hello World!"))
+	fmt.Println(Atoi("+1234"))
+	fmt.Println(Atoi("-1234"))
+	fmt.Println(Atoi("++1234"))
+	fmt.Println(Atoi("--1234"))
+}
