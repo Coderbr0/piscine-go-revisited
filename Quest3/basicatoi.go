d9f693e989241f566179f1c6b343918ad811233f
Coderbro <mo.abbaszadi@gmail.com>
2022-05-26 19:36:45 +0100

    basicatoi.go

--- Quest3/basicatoi.go
+++ Quest3/basicatoi.go
@@ -15,3 +15,21 @@ func main() {
 	fmt.Println(BasicAtoi("0000000012345"))
 	fmt.Println(BasicAtoi("000000"))
 }
+
+/* Alternative:
+
+func BasicAtoi(s string) int {
+	var result int
+
+	for _, num := range s {
+		// fmt.Println(num) => prints rune value
+		conversion := int(num) - 48 // casting num (rune value) to int; conversion is now int
+		// fmt.Println(result) => prints first value in iteration which is 0 from "var result int"
+		// fmt.Println(conversion) => prints int values
+		// result = (result * 10) + conversion // 0 + 1 = 1; then 10 + 2 = 12; then 120 + 3 = 123; then 1230 + 4 = 1234; 12340 + 5 = 12345
+		result = (result * 10) + conversion
+	}
+	return result
+}
+
+*/
