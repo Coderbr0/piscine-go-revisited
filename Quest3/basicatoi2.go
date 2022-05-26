8cdb0b38698c18f81bc5f30f80d64ef57e9c66aa
Coderbro <mo.abbaszadi@gmail.com>
2022-05-26 19:52:52 +0100

    basicatoi2.go

--- Quest3/basicatoi2.go
+++ Quest3/basicatoi2.go
@@ -6,11 +6,30 @@ import (
 )
 
 func BasicAtoi2(s string) int {
-
+	y, _ := strconv.Atoi(s)
+	return y
 }
 
-
-func main(){
-
-
+func main() {
+	fmt.Println(BasicAtoi2("12345"))
+	fmt.Println(BasicAtoi2("0000000012345"))
+	fmt.Println(BasicAtoi2("012 345"))
+	fmt.Println(BasicAtoi2("Hello World!"))
 }
+
+/* Alternative:
+
+func BasicAtoi2(s string) int {
+	var result int
+	for _, num := range s {
+		if num >= 48 && num <= 57 {
+		conversion := num - 48
+		result = (result * 10) + int(conversion)
+		} else {
+			return 0
+		}
+	}
+	return result
+}
+
+*/
