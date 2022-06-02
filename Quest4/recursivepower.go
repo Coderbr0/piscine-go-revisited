2c60038deccfcb2298228384d9fe4f102fc9614d
Coderbro <mo.abbaszadi@gmail.com>
2022-06-02 22:58:31 +0100

    recursivepower.go

---
+++ Quest4/recursivepower.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func recPower(nb, power int) int {
+	if power < 0 {
+		return 0
+	}
+	if power > 0 {
+		return nb * recPower(nb, power-1)
+	} 
+	return 1
+}
+
+func main() {
+	fmt.Println(recPower(4,3))
+}
