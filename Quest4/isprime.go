7cd4560a96a30e29d37910d206f43988241cf9a9
Coderbro <mo.abbaszadi@gmail.com>
2022-06-14 22:24:48 +0100

    isprime.go

--- Quest4/isprime.go
+++ Quest4/isprime.go
@@ -1,11 +1,20 @@
 package main
 
+import "fmt"
+
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
 	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 { 	// if modulus (remainder) equals zero; remainder of number divided by index equals zero
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-
+	fmt.Println(IsPrime(4))
+	fmt.Println(IsPrime(3))
 }
