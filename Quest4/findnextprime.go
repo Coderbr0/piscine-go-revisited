e2fc3cad17a8243548be6e1411ea6e87f3fc0c0e
Coderbro <mo.abbaszadi@gmail.com>
2022-06-21 22:55:51 +0100

    findnextprime.go

--- Quest4/findnextprime.go
+++ Quest4/findnextprime.go
@@ -2,19 +2,35 @@ package main
 
 import "fmt"
 
-func FindNextPrime(nb int) int {
-	if nb < 2 {
+func IsPrimeNum(nb int) bool {
+	if nb <= 1 {
 		return false
-
-		for i := 2; i < nb; i++ {
-			if nb%i == 0 { 
-				return false
-			}
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
 		}
 	}
+	return true
+}
+
+func FindNextPrime(Nb int) int {
+	if Nb <= 1 {
+		return 2
+	}
+	var prime int = Nb
+	var found bool = false
+	// Loop continuously until IsPrimeNum returns true for a number greater than nb
+	for !found {
+		prime++
+		if IsPrimeNum(prime) {
+			found = true
+		}
+	}
+	return prime
 }
 
 func main() {
+	fmt.Println(FindNextPrime(10))
 	fmt.Println(FindNextPrime(5))
-	fmt.Println(FindNextPrime(4))
 }
