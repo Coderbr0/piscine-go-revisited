683df4532049d5a62717cb689fedbfd2c8e3335e
Coderbro <mo.abbaszadi@gmail.com>
2022-06-25 22:48:06 +0100

    firstrune.go

--- Quest5/firstrune.go
+++ Quest5/firstrune.go
@@ -1,4 +1,17 @@
 package main
 
+import "github.com/01-edu/z01"
+
+func FirstRune(s string) {
+	sRune := []rune(s)
+	for _, v := range sRune {
+		z01.PrintRune(v)
+	}
+}
+
 func main() {
+	z01.PrintRune(FirstRune("Hello!"))
+	z01.PrintRune(FirstRune("Salut!"))
+	z01.PrintRune(FirstRune("Ola!"))
+	z01.PrintRune('\n')
 }
