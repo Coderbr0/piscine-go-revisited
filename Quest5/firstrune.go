73b3c361b399554bee2f1fea3b64a825311e552b
Coderbro <mo.abbaszadi@gmail.com>
2022-06-27 20:13:09 +0100

    firstrune.go

--- Quest5/firstrune.go
+++ Quest5/firstrune.go
@@ -2,11 +2,9 @@ package main
 
 import "github.com/01-edu/z01"
 
-func FirstRune(s string) {
+func FirstRune(s string) rune {
 	sRune := []rune(s)
-	for _, v := range sRune {
-		z01.PrintRune(v)
-	}
+	return sRune[0]
 }
 
 func main() {
@@ -15,3 +13,16 @@ func main() {
 	z01.PrintRune(FirstRune("Ola!"))
 	z01.PrintRune('\n')
 }
+
+/* Alternative:
+
+func FirstRune(s string) rune {
+sRune := []rune(s)
+for i := range sRune {
+z01.PrintRune(rune(i))
+}
+return sRune[0]
+}
+
+// for loop is unnecessary but code works
+*/
