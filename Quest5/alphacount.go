ca114d4a02805f156f1b932d0c396336fcec157d
Coderbro <mo.abbaszadi@gmail.com>
2022-07-11 21:27:47 +0100

    alphacount.go

--- Quest5/alphacount.go
+++ Quest5/alphacount.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	s := "Hello World"
+func AlphaCount(s string) int {
+	sRune := []rune(s)
 	count := 0
-	for i := 0; i < len(s); i++ {
-		fmt.Println(count)
+	for i := 0; i < len(sRune); i++ {
+		if (sRune[i] >= 65 && sRune[i] <= 90) || (sRune[i] >= 97 && sRune[i] <= 122) { // (rune value >= 65 && <= 90)  (ABC etc) || (rune value >= 97 && <= 122) (abc etc)
+			count++
+		}
 	}
+	return count
 }
 
-/*
-rune value >= 65 && <= 90  (ABC etc)
-rune value >= 97 && <= 122 (abc etc)
+func main() {
+	s := "Hello 78 World!    4455 /"
+	nb := AlphaCount(s)
+	fmt.Println(nb)
+}
+
+/* Alternative:
+
+func main() {
+	s := "Hello 78 World!    4455 /"
+	sRune := []rune(s)
+	count := 0
+	for i := 0; i < len(sRune); i++ {
+		if sRune[i] >= 65 && sRune[i] <= 90 || sRune[i] >= 97 && sRune[i] <= 122 {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
+
 */
