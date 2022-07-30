17cb123054e295628bd9d1d99b46266e1b45d897
Coderbro <mo.abbaszadi@gmail.com>
2022-07-30 01:06:23 +0100

    isupper.go

--- Quest5/isupper.go
+++ Quest5/isupper.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"unicode"
 )
 
 func IsUpper(s string) bool {
-runeTheString := []rune(s)
-fmt.Println(runeTheString)
-for i := 0; i < len(runeTheString); i++ {
-if (runeTheString[i] >= 65 && runeTheString[i] <= 90) { // (rune value >= 65 && <= 90)  (ABC etc)
-return true
-}
-}
-return false
+	for _, v := range s {
+		fmt.Println(v)
+		if !unicode.IsUpper(v) && unicode.IsLetter(v) {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-fmt.Println(IsUpper("HELLO"))
-fmt.Println(IsUpper("HELLO!"))
-fmt.Println(IsUpper("HeLLO!"))
+	fmt.Println(IsUpper("HELLO"))
+	fmt.Println(IsUpper("HELLO!"))
+	fmt.Println(IsUpper("HELlO!"))
 }
-
-/* Note: Prints true at any value found within the range; if any value is greater than 65 and less than 90 */
